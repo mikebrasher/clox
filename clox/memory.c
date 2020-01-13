6839b57220f293c30f191913f084b8dffc31dcc6
Mike Brasher <33470429+mikebrasher@users.noreply.github.com>
2020-01-12 18:53:15 -0600

    Chapter 27: Classes and Instances
    Added class and instance object types and the ability to create/free them.  Added property getter/setter to access dynamically typed state data.

--- clox/memory.c
+++ clox/memory.c
@@ -96,6 +96,14 @@ static void blackenObject(Obj* object)
 		break;
 	}
 
+	case OBJ_INSTANCE:
+	{
+		ObjInstance* instance = (ObjInstance*)object;
+		markObject((Obj*)instance->klass);
+		markTable(&instance->fields);
+		break;
+	}
+
 	case OBJ_UPVALUE:
 		markValue(((ObjUpvalue*)object)->closed);
 		break;
@@ -114,6 +122,13 @@ static void freeObject(Obj* object)
 	
 	switch (object->type)
 	{
+	case OBJ_CLASS:
+	{
+		ObjClass* klass = (ObjClass*)object;
+		markObject((Obj*)klass->name);
+		break;
+	}
+
 	case OBJ_CLOSURE:
 	{
 		ObjClosure* closure = (ObjClosure*)object;
@@ -126,6 +141,14 @@ static void freeObject(Obj* object)
 		ObjFunction* function = (ObjFunction*)object;
 		freeChunk(&function->chunk);
 		FREE(ObjFunction, object);
+		break;
+	}
+
+	case OBJ_INSTANCE:
+	{
+		ObjInstance* instance = (ObjInstance*)object;
+		freeTable(&instance->fields);
+		FREE(ObjInstance, object);
 		break;
 	}
 
